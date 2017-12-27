e8f6ca0caacdb7963a1cc3fd0aa45635b30ff4c5
arigo16 <arigo1602@gmail.com>
2017-12-27 15:54:54 +0700

    aa

--- act/apup2.php
+++ act/apup2.php
@@ -1,11 +1,13 @@
 <?php
 
 include '../include/config.php';
-
+session_start();
+$appfin = $_SESSION['username'];
 $sts = $_POST['sts'];
 $id = $_GET['id'];
+$sdate = DATE('Y-m-d');
 
-$db = $con->query("UPDATE pengajuan_tb SET app_finance='$sts' WHERE id='$id'");
+$db = $con->query("UPDATE pengajuan_tb SET app_finance='$sts', AppDateFin='$sdate', AppFin='$appfin' WHERE id='$id'");
 if ($con->affected_rows > 0){
     header("location:../index.php?page=appfin");
 }
